fa9cec4e6019155bbd157a7c3ae3fe69a7d8451a
Funmilayo Oladipo <126979205+Lightstone1@users.noreply.github.com>
2024-09-07 05:53:43 +0100

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -30,10 +30,11 @@ ORDER BY year, quarter, sales_rank;
 
 
 
-
+select * from customer
 select * from exchange
 select * from product
-select * from sales;
+select * from sales
+select * from stores;
 
 /* QUESTION 2: What are the most profitable products in each region, and how do exchange rate
 fluctuations impact profitability?
@@ -67,3 +68,40 @@ Select
  	RANK() OVER (PARTITION BY Region ORDER BY profit DESC) AS Rank_Profit
 From
 	Profit_Calculation
+
+
+/*Which customer segments exhibit the highest repeat purchase behavior, and what
+are the key factors driving loyalty?
+○ Determine which customer demographics are most loyal and analyze the factors
+contributing to repeat purchases.*/
+
+WITH Repeat_Customers AS (
+    SELECT 
+        "CustomerKey",
+        COUNT("Order Number") AS Purchase_Count
+    FROM 
+        public.sales
+    GROUP BY 
+        "CustomerKey"
+    HAVING COUNT("Order Number") > 1  -- Identify customers with more than one purchase--
+)
+SELECT 
+    customer."Gender",
+    customer."Age",
+    customer."Country",
+    customer."Continent",
+    Repeat_Customers.Purchase_Count,
+    AVG(sales."Quantity") AS Avg_Purchase_Quantity,
+    AVG(sales."Quantity" * product."Unit Price USD") AS Avg_Spending
+FROM 
+    Repeat_Customers
+JOIN 
+    public.customer ON Repeat_Customers."CustomerKey" = customer."CustomerKey"
+JOIN 
+    public.sales ON Repeat_Customers."CustomerKey" = sales."CustomerKey"
+JOIN 
+    public.product  ON sales."ProductKey" = product."ProductKey"
+GROUP BY 
+    customer."Gender", customer."Age", customer."Country", customer."Continent", Repeat_Customers.Purchase_Count
+ORDER BY 
+    Repeat_Customers.Purchase_Count DESC, Avg_Spending DESC;
